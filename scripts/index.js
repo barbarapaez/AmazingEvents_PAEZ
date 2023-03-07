7c5714f65b952507bbac802d3502de37c84b17cc
barbarapaez <barbaraepaez97@outlook.com>
2023-03-07 10:49:45 -0300

    estilo de las cards, ruta para details desde index

--- scripts/index.js
+++ scripts/index.js
@@ -13,7 +13,7 @@ events.forEach((event) => {
         <p class="card-text">${event.description}</p>
         <div class="card-footer bg-transparent d-inline-flex">
             <p class="card-text price">Price $${event.price}</p>
-            <a href="./details.html" class="btn card-btn">
+            <a href="./pages/details.html" class="btn card-btn">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
                     <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
                 </svg>
