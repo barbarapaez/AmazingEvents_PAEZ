3d7db74f64546354418c4bfd9746a26c8568a977
barbarapaez <barbaraepaez97@outlook.com>
2023-03-14 18:22:58 -0300

    página de details generandose (incompleta)

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 import data from "./amazing.js"
-import  {Cards, detailPage} from "./functions.js";
+import  {Cards} from "./functions.js";
 
 let element = document.getElementById("cardElement");
 Cards(data.events, element);
