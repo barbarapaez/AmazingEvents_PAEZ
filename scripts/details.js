bbb4d54e455e8094153dc1992002e83bdc4704a0
barbarapaez <barbaraepaez97@outlook.com>
2023-03-14 21:54:20 -0300

    páginas de details funcionando

--- scripts/details.js
+++ scripts/details.js
@@ -10,22 +10,24 @@ events = data.events
 console.log(events)
 const event = events.find(element => element._id == id);
 
+const element = document.getElementById("detailCard");
+let fragment = document.createDocumentFragment();
+let div = document.createElement("div");
+div.innerHTML = `
+<div class="img-container d-flex justify-content-center">
+                <img src="${event.image}" alt="S${event.description}">
+            </div>
+            <div class="description mx-auto">
+                <h5>${event.name}</h5>
+                <h6>Date: ${event.date}</h6>
+                <h6>Description: ${event.description}</h6>
+                <h6>Category: ${event.category}</h6>
+                <h6>Place: ${event.place}</h6>
+                <h6>Capacity: ${event.capacity}</h6>
+                <h6>Assistance or estimate: ${event.assistance}</h6>
+                <h6>Price: ${event.price}</h6>
+            </div>`
+fragment.appendChild(div);
+element.appendChild(fragment);
 
-const fragment = document.createDocumentFragment();
-    const div = document.createElement("div");
-    div.innerHTML = `
-    <div class="img-container d-flex justify-content-center">
-        <img src="${event.image}" alt="Screening of La la land in a patio setting">
-    </div>
-    <div class="description mx-auto">
-        <h5>Cinema/Example</h5>
-        <h6>Date: ${event.date}</h6>
-        <h6>Description: ${event.description}</h6>
-        <h6>Category: ${event.category}</h6>
-        <h6>Place: ${event.place}</h6>
-        <h6>Capacity: ${event.capacity}</h6>
-        <h6>Assistance or estimate: ${event.assistance}</h6>
-        <h6>Price: ${event.price}</h6>
-    </div>
-    `
-    container.appendChild(fragment);
+
