66f0dea4ec29e42b5570071676d2d80b070dd168
barbarapaez <barbaraepaez97@outlook.com>
2023-03-22 00:19:07 -0300

    trabajo sobre la tabla

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,5 +1,7 @@
 //CONSTANTE PARA SEÑALAR DÓNDE ESTOY TRABAJANDO, PRIMERA TABLA
 const tableOne = document.getElementById("firstTable")
+const tableTwo = document.getElementById("secondTable")
+const tableThree = document.getElementById("thirdTable")
 
 //TRAÍDA DE FUNCIONES NECESARIAS
 import  {pastEvents, upcomingEvents} from "./functions.js";
@@ -19,20 +21,24 @@ function getEvents(){
         let sortedPastEvents = pastEvents(eventList, currentDate)
         let sortedUpcomingEvents = upcomingEvents(eventList, currentDate)
         drawTd(eventStatistics(sortedPastEvents, eventList), tableOne)
-        upcomingEventsStatistics(sortedUpcomingEvents)
-/*         getCategories(eventList)
-        categoriesStatistics(eventList, getCategories(eventList))
-        drawTr((eventStatistics(pastEvents(eventList, currentDate),eventList)), tableOne) */
+        let upcomingStats = eventsStats(sortedUpcomingEvents)
+        for (let key in upcomingStats) {
+            drawTd(upcomingStats[key], tableTwo)
+        }
+        let pastStats = eventsStats(sortedPastEvents)
+        for (let key in pastStats) {
+            drawTd(pastStats[key], tableThree)
+        }
 
-    }).catch(err=>console.error(err))
+
+}).catch(err=>console.error(err))
 } getEvents()
 
-//FUNCIÓN QUE, DADOS DOS ARRAYS (ARR1=LISTA DE EVENTOS; ARR2=CATEGORIAS) DEVUELVE STATS PARA LA PRIMERA TABLA COMO UN OBJETO
+//FUNCIÓN QUE, DADOS DOS ARRAYS (ARR1=LISTA DE EVENTOS PASADOS; ARR2=LISTA DE EVENTOS COMPLETA) DEVUELVE STATS PARA LA PRIMERA TABLA COMO UN OBJETO
 function eventStatistics(arr1, arr2) {
-    let highestAttendenceEvent = arr1.reduce((prev, current) => (prev.assistance > current.assistance) ? prev : current).name
-    let lowestAttendenceEvent = arr1.reduce((prev, current) => (prev.assistance < current.assistance) ? prev : current).name
+    let highestAttendenceEvent = arr1.reduce((prev, current) => (prev.assistance/prev.capacity > current.assistance/current.capacity) ? prev : current).name
+    let lowestAttendenceEvent = arr1.reduce((prev, current) => (prev.assistance/prev.capacity < current.assistance/current.capacity) ? prev : current).name
     let capacity = arr2.reduce((prev, current) => (prev.capacity > current.capacity) ? prev : current).name
-    /* arr.sort((a, b) => a.capacity - b.capacity)[0] */
     let result =  {
         highestAttendenceEvent: highestAttendenceEvent,
         lowestAttendenceEvent: lowestAttendenceEvent, 
@@ -43,31 +49,57 @@ function eventStatistics(arr1, arr2) {
 
 //FUNCIÓN PARA DIBUJAR LA TABLA
 function drawTd(data, container){
-    container.innerHTML = ''
-    let fragment = document.createDocumentFragment()
+/*     let fragment = document.createDocumentFragment()
+ */    let fragment = document.createElement("tr")
     for (let key in data) {
         let td = document.createElement("td")
         td.classList = ""
         td.innerHTML = 
         `${data[key]}` 
         fragment.appendChild(td)
-    } container.appendChild(fragment)
+    } 
+/*     container.appendChild(fragment)
+ */    container.parentNode.insertBefore(fragment, container)
 }
 
 //FUNCIÓN QUE DA COMO RESULTADO UN ARRAY, DEVUELVE LAS CATEGORIAS DE TODOS LOS EVENTOS, SIN REPETIR
 function getCategories (arr){
-    let categories = [...new Set((arr.map(item => item.category).flat()))]
+    let categories = [...new Set(arr.map(item => item.category).flat())]
 /*     let categories = [...new Set((arr.map(item => item.category).flat()).map(item => item.toLowerCase()))]
  */    
     return categories
 }
 
-//FUNCIÓN PARA OBTENER LAS ESTADÍSTICAS POR CATEGORÍA DE LOS UPCOMING EVENTS
+//FUNCIÓN QUE DADO UN ARRAY DE EVENTOS DEVUELVE UN OBJETO DONDE LAS CLAVES SON LAS CATEOGORÍAS Y LOS VALORES SON SU GANANCIA, CAPACIDAD ETC
+function eventsStats(events) {
+    let categories = getCategories(events)
+    let stats = {}
+    //creación de valores inciiales para attendance, revenue y capacity dentro de cada categoría dentro del objeto stats
+    categories.forEach((category) => {
+        stats[category] = {
+            category: category,
+            revenue: 0,
+            attendance: 0,
+            capacity: 0
+        }
+    })
+    events.forEach((event) => {
+        //utilizar attendance si existe, sino estimate (pensando en la diferencia entre past y upcoming events)
+        let eventAttendance = event.assistance ? event.assistance : event.estimate
+        let eventRevenue = eventAttendance * event.price
+        let eventCapacity = event.capacity
+        let category = event.category
+        stats[category].attendance += eventAttendance
+        stats[category].revenue += eventRevenue
+        stats[category].capacity += eventCapacity
+    })
+    //determinar attendancePercentage en base al promedio de todas las attendance y capacities
+    categories.forEach((category) => {
+        stats[category].attendancePercentage = ((stats[category].attendance/stats[category].capacity)*100).toFixed(2)
+        delete stats[category].attendance
+        delete stats[category].capacity
+    })
+    return stats
+}
 
-
-//arr 1= array de eventos; arr 2 = array de categorias
-//for each de cada genero, y voy viendo de este género en particular qué está pasando
-/* function categoriesStatistics(arr1, arr2){
-    let eventsForEachCategory = arr1.filter(item => arr2.includes(item.category[0]).toLowerCase())
-    console.log(eventsForEachCategory)
-} */
+//
