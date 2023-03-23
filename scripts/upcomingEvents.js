788d7b4f4e8d9433566cffcaf8b0ec98f42381a6
barbarapaez <barbaraepaez97@outlook.com>
2023-03-23 09:17:54 -0300

    upcoming events funcionando con asincronismo

--- scripts/upcomingEvents.js
+++ scripts/upcomingEvents.js
@@ -1,42 +1,28 @@
-import data from "./amazing.js"
-import  {upcomingEvents, cards, createCheckboxes} from "./functions.js";
-let element = document.getElementById("cardElement");
-cards(upcomingEvents(data.events, data.currentDate), element);
-let checkboxElement = document.getElementById("checkboxCreation");
-createCheckboxes(data.events, checkboxElement);
+import  {cards, createCheckboxes, upcomingEvents, checkboxFilter, searchBarFilter} from "./functions.js";
+
+const element = document.getElementById("cardElement");
+let eventList = []
+let currentDate
+const checkboxElement = document.getElementById("checkboxCreation");
 const input = document.querySelector('.input')
-console.log(input)
-let upcomingEventsArr = [];
-upcomingEventsArr = upcomingEvents(data.events, data.currentDate);
 
-input.addEventListener('input', doubleFilter);
-checkboxElement.addEventListener('change', doubleFilter);
 
-//FUNCIÓN PARA LAS CHECKBOXES
-function checkboxFilter(arr){
-    let checkboxes = document.querySelectorAll("input[type='checkbox']")
-    let checkboxesArr = Array.from(checkboxes);
-    let checkedCategories = checkboxesArr.filter(check => check.checked);
-    let checkedCategoryValue = checkedCategories.map(checkChecked => checkChecked.value)
-    let filteredArr = arr.filter(event => checkedCategoryValue.includes(event.category))
-    if(checkedCategories.length>0){
-        return filteredArr
-    } else {
-        return arr
-    }
-}
-
-//FUNCIÓN PARA LA SEARCH BAR
-function searchBarFilter(arr, text){
-    let filteredArr = arr.filter(event => event.name.toLowerCase().includes(text.toLowerCase()))
-    return filteredArr
-}
-
-//FUNCIÓN DE DOBLE FILTRADO
-function doubleFilter(){
-    let primerFiltro = checkboxFilter(upcomingEventsArr)
-    let checksAndTextFilter = searchBarFilter(primerFiltro, input.value);
-    console.log(checksAndTextFilter)
-    cards(checksAndTextFilter, element)
-}
-
+function getEvents(){
+    fetch("../data/amazing.json")
+    .then(response => response.json())
+    .then(data => {
+        eventList = data.events
+        currentDate = data.currentDate
+        let sortedUpcomingEvents = upcomingEvents(eventList, currentDate)
+        cards(sortedUpcomingEvents, element)
+        createCheckboxes(sortedUpcomingEvents, checkboxElement)
+        input.addEventListener('input', doubleFilter);
+        checkboxElement.addEventListener('change', doubleFilter);
+            //FUNCIÓN DE DOBLE FILTRADO
+            function doubleFilter(){
+                let checkboxFilteredArray = checkboxFilter(sortedUpcomingEvents)
+                let checksAndTextFilter = searchBarFilter(checkboxFilteredArray, input.value);
+                cards(checksAndTextFilter, element)
+            }
+    }).catch(err=>console.error(err))
+} getEvents()
